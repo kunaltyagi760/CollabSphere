66a5526eefe0a073de35c7bf71b57886be981da5
Kunal Tyagi <kunaltyagi760@gmail.com>
2025-03-01 06:10:21 +0530

    login and signup completed

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ message: "Access Denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = decoded.userId;
+    req.body.username = decoded.name;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Invalid Token" });
+  }
+};
